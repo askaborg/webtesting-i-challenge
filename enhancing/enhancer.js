2b89beda2fe4e4aa8668527813de31cb5b2d6643
askaborg <hienle@askaborg.com>
2020-06-11 18:23:16 -0700

    Great Swords

--- enhancing/enhancer.js
+++ enhancing/enhancer.js
@@ -1,22 +1,43 @@
 module.exports = {
-  succeed,
-  fail,
-  repair,
-  get,
-};
+    succeed,
+    fail,
+    repair,
+    get,
+}
 
 function succeed(item) {
-  return { ...item };
+    return {
+        ...item,
+        enhancement: item.enhancement === 20 ? 20 : item.enhancement + 1
+    }
 }
 
 function fail(item) {
-  return { ...item };
+    const newEnhancement =
+        item.enhancement > 16 ? item.enhancement - 1 : item.enhancement
+    let newDurability =
+        item.enhancement < 15 ? item.durability - 5 : item.durability - 10
+    newDurability = newDurability < 0 ? 0 : newDurability
+
+    return {
+        ...item,
+        enhancement: newEnhancement, durability: newDurability
+    }
 }
 
 function repair(item) {
-  return { ...item };
+    return { ...item, durability: 100 }
 }
 
 function get(item) {
-  return { ...item };
+    let newName = item.name
+    if (item.enhancement > 0) {
+        if (item.name[0] !== "[") {
+        newName = `[+${item.enhancement}] ${item.name}`
+        } else {
+        newName = `[+${item.enhancement}]` + item.name.split("]")[1]
+        }
+    }
+    
+    return { ...item, name: newName }
 }
